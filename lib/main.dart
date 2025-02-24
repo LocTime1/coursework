b76352fe4f57f0fac9f03a2d1a3d547ae7153884
Гаврилов Александр Артёмович <alargavrilov@edu.hse.ru>
2025-02-24 21:11:36 +0300

    Начал переделывать весь дизайн

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Убирает отладочный баннер
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
@@ -24,325 +25,207 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    int year = now.year;
+    String monthName = DateFormat('MMMM').format(now);
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.green.shade300, // Верхний зелёный цвет
-              Colors.white, // Плавный переход в белый
-            ],
+      body: Stack(
+        children: [
+          // Фон
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Color.fromRGBO(64, 67, 201, 1),
           ),
-        ),
-        child: Column(
-          children: [
-            SizedBox(height: 60),
-            MyAppBar(),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                SizedBox(
-                  width: 20,
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.28,
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.72,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
                 ),
-                AnalyzWidget(),
-                SizedBox(
-                  width: 20,
-                ),
-                AllMenus(),
-              ],
+              ),
             ),
-            SizedBox(
-              height: 20,
+          ),
+          Positioned(top: 45, left: 6, child: MenuButton()),
+          Positioned(top: 55, right: 13, child: AddButton()),
+          Positioned(
+            top: 110,
+            left: 15,
+            child: Text("$monthName, $year",
+                style: TextStyle(color: Colors.white, fontSize: 22)),
+          ),
+          Positioned(
+            top: 150,
+            left: 0,
+            right: 0, // Добавляем ограничения по ширине
+            child: SizedBox(
+              height: 80, // Высота контейнера с датами
+              child: DatesWidget(),
             ),
-            Button()
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
 
-class AllMenus extends StatelessWidget {
-  const AllMenus({super.key});
+class DatesWidget extends StatefulWidget {
+  const DatesWidget({super.key});
+
+  @override
+  _DatesWidgetState createState() => _DatesWidgetState();
+}
+
+class _DatesWidgetState extends State<DatesWidget> {
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 175,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.white,
-                Colors.green.shade200,
-              ],
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10,
-                offset: Offset(3, 6),
-              ),
-            ],
-          ),
-          child: Center(
-            child: Text(
-              "Задачи на сегодня",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 30,
-        ),
-        Container(
-          width: 175,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.white,
-                Colors.green.shade200,
-              ],
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10,
-                offset: Offset(3, 6),
-              ),
-            ],
-          ),
-          child: Center(
-            child: Text(
-              "Все задачи",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 30,
-        ),
-        Container(
-          width: 175,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.white,
-                Colors.green.shade200,
-              ],
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10,
-                offset: Offset(3, 6),
-              ),
-            ],
-          ),
-          child: Center(
-            child: Text(
-              "Выполнено",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+    DateTime now = DateTime.now();
+    List<DateTime> dates = _getDates(now);
+
+    // Вычисляем индекс текущей даты
+    int todayIndex = dates.indexWhere((date) =>
+        date.year == now.year &&
+        date.month == now.month &&
+        date.day == now.day);
+
+    // Прокручиваем к текущей дате после сборки
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        // Сдвигаем позицию так, чтобы текущая дата была по центру
+        double offset =
+            todayIndex * 86.0 - MediaQuery.of(context).size.width / 2 + 70.0;
+        _scrollController.animateTo(offset,
+            duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+      }
+    });
+
+    return SingleChildScrollView(
+      controller: _scrollController,
+      scrollDirection: Axis.horizontal, // Горизонтальная прокрутка
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center, // Центрируем список
+        children: dates.map((date) => _buildContainer(date, now)).toList(),
+      ),
+    );
+  }
+
+  List<DateTime> _getDates(DateTime currentDate) {
+    List<DateTime> dates = [];
+    for (int i = -7; i <= 7; i++) {
+      // Показываем 7 дней до и 7 после текущей даты
+      dates.add(currentDate.add(Duration(days: i)));
+    }
+    return dates;
+  }
+
+  Widget _buildContainer(DateTime date, DateTime currentDate) {
+    bool isToday = date.year == currentDate.year &&
+        date.month == currentDate.month &&
+        date.day == currentDate.day;
+
+    return Container(
+      width: 70,
+      height: 100, // Увеличиваем высоту, чтобы добавить день недели
+      margin: EdgeInsets.symmetric(horizontal: 7),
+      decoration: BoxDecoration(
+        color: isToday ? Colors.white : Color.fromRGBO(89, 95, 255, 1),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Сокращённое название дня недели
+          Text(
+            DateFormat('E')
+                .format(date)
+                .toUpperCase(), // Сокращение дня недели (например, "Mon", "Tue")
+            style: TextStyle(
+              color: isToday ? Color.fromRGBO(89, 95, 255, 1) : Colors.white,
+              fontSize: 14, // Меньший размер шрифта для дня недели
             ),
           ),
-        )
-      ],
+          // Число
+          Text(
+            DateFormat('d').format(date), // Отображаем только число
+            style: TextStyle(
+              color: isToday
+                  ? Color.fromRGBO(89, 95, 255, 1)
+                  : Colors.white, // Белый для текущего дня
+              fontSize: 18,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class AnalyzWidget extends StatelessWidget {
-  const AnalyzWidget({super.key});
+class MenuButton extends StatelessWidget {
+  const MenuButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        // Отступ сверху (например, для статус-бара)
-        width: 180,
-        height: 420,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25),
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.white, // Верхняя часть белая
-              Colors.green.shade200, // Плавный переход в зелёный
-            ],
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 10,
-              offset: Offset(3, 6),
-            ),
+        width: 60,
+        height: 80,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(top: 18, left: 15, child: _line()),
+            Positioned(top: 28, child: _line(longer: true)),
+            Positioned(top: 38, right: 15, child: _line()),
           ],
         ));
   }
-}
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _line({bool longer = false}) {
     return Container(
-        // Отступ сверху (например, для статус-бара)
-        width: 380,
-        height: 250,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25),
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.white, // Верхняя часть белая
-              Colors.green.shade200, // Плавный переход в зелёный
-            ],
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 10,
-              offset: Offset(3, 6),
-            ),
-          ],
-        ),
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-          // Кнопка Напоминания
-          // Кнопка настройки
-          Container(
-            width: 90,
-            height: 90,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.white,
-                  Colors.green.shade100,
-                ],
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 10,
-                  offset: Offset(3, 6),
-                ),
-              ],
-            ),
-            child: Center(
-              child: Icon(Icons.notifications, color: Colors.green, size: 40),
-            ),
-          ),
-
-          // Кнопка "Tasks" (круглая)
-          Container(
-            width: 120,
-            height: 120,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.white,
-                  Colors.green.shade100,
-                ],
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 10,
-                  offset: Offset(3, 6),
-                ),
-              ],
-            ),
-            child: Center(
-              child: Text(
-                "Задачи",
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black87,
-                ),
-              ),
-            ),
-          ),
-
-          // Кнопка настройки
-          Container(
-            width: 90,
-            height: 90,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.white,
-                  Colors.green.shade100,
-                ],
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 10,
-                  offset: Offset(3, 6),
-                ),
-              ],
-            ),
-            child: Center(
-              child: Icon(Icons.settings, color: Colors.green, size: 40),
-            ),
-          ),
-        ]));
+      width: longer ? 25 : 13, // Средняя полоска длиннее
+      height: 4,
+      decoration: BoxDecoration(
+        color: Colors.white, // Цвет полосок
+        borderRadius: BorderRadius.circular(2),
+      ),
+    );
   }
 }
 
-class Button extends StatelessWidget {
-  const Button({super.key});
+class AddButton extends StatelessWidget {
+  const AddButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 380,
-        height: 70,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25),
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.white, // Верхняя часть белая
-              Colors.green.shade200, // Плавный переход в зелёный
-            ],
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 10,
-              offset: Offset(3, 6),
-            ),
-          ],
-        ));
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(15)),
+      child: Icon(
+        Icons.add,
+        color: Color.fromRGBO(64, 67, 201, 1),
+        size: 35,
+      ),
+    );
   }
 }

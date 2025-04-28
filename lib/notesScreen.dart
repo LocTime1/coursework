2248bb180e4d36db1843a69d5ed498100a9ce0bc
Гаврилов Александр Артёмович <alargavrilov@edu.hse.ru>
2025-04-28 22:47:53 +0300

    Сделал вывод задач в столбиках

--- lib/notesScreen.dart
+++ lib/notesScreen.dart
@@ -1,8 +1,10 @@
-// ignore_for_file: prefer_const_constructors, curly_braces_in_flow_control_structures
+// ignore_for_file: prefer_const_constructors, curly_braces_in_flow_control_structures, prefer_const_literals_to_create_immutables
+
+import 'dart:math';
 
 import 'package:coursework/main.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'widgets/TasksScreen/menuButtonWidget.dart';
 
 class NotesScreen extends StatefulWidget {
@@ -13,18 +15,61 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
+  final List<Map<String, String>> notes = [
+    {
+      'title': 'Курс по Flutter',
+      'subtitle': 'Пройти раздел о сетках и адаптивности',
+    },
+    {
+      'title': 'Список покупок',
+      'subtitle':
+          'Молоко, яйца, хлеб, сыр апааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа',
+    },
+    {
+      'title': 'Заметки для курсовой',
+      'subtitle': 'Введение и литература',
+    },
+    {
+      'title': 'Позвонить Олегу',
+      'subtitle': 'Обсудить поездку на выходные',
+    },
+    {
+      'title': 'Написать кому-то',
+      'subtitle': 'Обсудить поездку на выходные',
+    },
+    {
+      'title': 'Позвонить маме',
+      'subtitle': 'Обсудить поездку на выходные пп п папапаееаеаееа',
+    },
+    {
+      'title': 'Пihsfdyyshfhousd',
+      'subtitle':
+          'Обсудить поездку на выходныеры ывраргывгапгывпгаывпашлнгывангшыванпывпашнпывапгщывнаишывнпагнывпншыпншапшнпш',
+    },
+  ];
+
+  // Список доступных цветов
+  final List<Color> availableColors = [
+    // Color.fromRGBO(166, 152, 237, 1),
+    // Color.fromRGBO(127, 188, 249, 1),
+    // Color.fromRGBO(128, 200, 194, 1),
+    // Color.fromRGBO(235, 171, 159, 1),
+    Colors.white
+  ];
+
+  final Random random = Random();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(128, 200, 194, 1),
       body: Stack(children: [
         Positioned(
-          top: MediaQuery.of(context).size.height * 0.28,
+          top: MediaQuery.of(context).size.height * 0.23,
           left: 0,
           right: 0,
           child: Container(
             width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.72,
+            height: MediaQuery.of(context).size.height * 0.77,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.only(
@@ -54,6 +99,81 @@ class _NotesScreenState extends State<NotesScreen> {
             ),
           ),
         ),
+        Positioned(
+            top: 55,
+            right: 13,
+            child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Icon(
+                    Icons.add,
+                    color: Color.fromRGBO(128, 200, 194, 1),
+                    size: 35,
+                  ),
+                ))),
+        Positioned(
+            top: 110,
+            left: 65,
+            child: Text(
+              " Your notes",
+              style: TextStyle(color: Colors.white, fontSize: 53),
+            )),
+        Positioned(
+            top: 200,
+            left: MediaQuery.of(context).size.width * 0.05,
+            child: Container(
+                height: 700,
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: MasonryGridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  itemCount: notes.length,
+                  itemBuilder: (context, index) {
+                    final note = notes[index];
+                    final color =
+                        availableColors[random.nextInt(availableColors.length)];
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: color.withOpacity(0.6),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 8,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            note['title']!,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            note['subtitle']!,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                )))
       ]),
       drawer: SizedBox(
         width: 70,
